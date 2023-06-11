dcf3333beb7faa1cfa2e44f22426473ce0445200
John Mampilli <jsmampilli@gmail.com>
2023-06-11 13:32:18 +0200

    Functional __init__(), derived class to exclude boundaries, reduce dataset

--- dataset.py
+++ dataset.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import torch
-import numpy  # Required only to check return types?
-from typing import Union
+import numpy as np  # Required for possible float-int comparisons, return types
+from typing import Union, Optional
 
 
 class PINN_Dataset(torch.utils.data.Dataset):
@@ -23,21 +23,83 @@ class PINN_Dataset(torch.utils.data.Dataset):
 
         input_cols, output_cols must be lists to guarantee numpy.ndarray is returned
         """
-        data = pd.read_csv(filename)
+        data = self._read_data(filename)
+        # Split data into inputs and outputs
+        self._inputs, self._outputs = self._split_inputs_outputs(data, input_cols, output_cols)
+
+    def _read_data(self, filename: str) -> pd.DataFrame:
+        return pd.read_csv(filename)
+
+    def _split_inputs_outputs(self,
+                              data: pd.DataFrame,
+                              input_cols: Union[list[str], list[int]],
+                              output_cols: Union[list[str], list[int]]
+                              ) -> tuple[np.ndarray, np.ndarray]:
+
         # try-catch block to access columns in .csv by either names or indices
         try:
             # data.loc inputs string labels (column names)
-            self._inputs, self._outputs = [data.loc[:, labels].to_numpy() for
-                                           labels in (input_cols, output_cols)]
+            inputs, outputs = [data.loc[:, labels].to_numpy() for
+                               labels in (input_cols, output_cols)]
         except KeyError:
             # data.iloc expects int indices
-            self._inputs, self._outputs = [data.iloc[:, labels].to_numpy() for
-                                           labels in (input_cols, output_cols)]
+            inputs, outputs = [data.iloc[:, labels].to_numpy() for
+                               labels in (input_cols, output_cols)]
 
-        assert len(self._inputs) == len(self._outputs)
+        assert len(inputs) == len(outputs)
+        return (inputs, outputs)
 
     def __len__(self) -> int:
         return len(self._inputs)
 
-    def __getitem__(self, idx: int) -> tuple[numpy.ndarray, numpy.ndarray]:
+    def __getitem__(self, idx: int) -> tuple[np.ndarray, np.ndarray]:
         return (self._inputs[idx], self._outputs[idx])
+
+
+class Interior_Partial_Dataset(PINN_Dataset):
+    """
+    Reads data (for data loss) from .csv files, intended for use
+    with torch.utils.data.Dataloader.
+
+    Excludes boundary points from read data if required.
+
+    Can use only partial data from the .csv, skipping over a specified number of
+    lines to reduce the size of the dataset.
+
+    Returns data as (input array, output array), where inputs and outputs are
+    wrt the PINN model.
+    """
+    def __init__(self,
+                 filename: str,
+                 input_cols: Union[list[str], list[int]],
+                 output_cols: Union[list[str], list[int]],
+                 exclude_bounds: Optional[list[tuple[float, float]]] = None,
+                 sampling_fraction: Optional[float] = None):
+        """
+        filename: .csv file containing data
+        input_cols, output_cols: column names or column indices of input and
+        output data in the .csv file
+        sampling_interval: number of samples to skip after each line in .csv
+        exclude_bounds: List of tuples containing domain bounds to exclude from dataset,
+        in order to sample only interior points
+
+        input_cols, output_cols must be lists to guarantee numpy.ndarray is returned
+        """
+        data = super()._read_data(filename)
+        # Exclude boundary points if bounds are specified
+        if exclude_bounds is not None:
+            data = self._exclude_boundaries(data, exclude_bounds)
+        # Reduce dataset if specified
+        if sampling_fraction is not None:
+            data = data.iloc[::sampling_fraction, :]
+        # Split data into inputs and outputs
+        self._inputs, self._outputs = super()._split_inputs_outputs(data, input_cols, output_cols)
+
+    def _exclude_boundaries(self,
+                            data: pd.DataFrame,
+                            exclude_bounds: list[tuple[float, float]]) -> pd.DataFrame:
+        for axis_idx, axis in enumerate(exclude_bounds):
+            for bound in axis:
+                data.drop(data[np.isclose(data.iloc[:, axis_idx], bound)].index,
+                          inplace=True)
+        return data
