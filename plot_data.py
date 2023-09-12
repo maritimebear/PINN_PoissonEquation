e490016f567a7bb11ff396e52b9cc865627e5e34
John Mampilli <jsmampilli@gmail.com>
2023-09-12 12:27:20 +0200

    Shortened title on reference data plot

--- plot_data.py
+++ plot_data.py
@@ -16,11 +16,11 @@ ax = fig.add_subplot(1, 1, 1)
 cs = ax.contourf(*[arr.reshape(n, n) for arr in (inputs[:, 0], inputs[:, 1], output)])
 cbar = fig.colorbar(cs)
 
-ax.set_title("Reference solution (ground truth)\n" +
-             r"$ -\Delta u(x,y) = 2 \pi ^{2} cos(\pi x) cos(\pi y) $ in $ \Omega $" + "\n" +
-             r"$ u(x, y) = cos(\pi x) cos(\pi y) $ on $ \partial \Omega $" + "\n" +
-             r"$ \Omega = (0, 1) \times (0, 1) $"
-             )
+ax.set_title("Reference solution (ground truth)\n") # +
+#              r"$ -\Delta u(x,y) = 2 \pi ^{2} cos(\pi x) cos(\pi y) $ in $ \Omega $" + "\n" +
+#              r"$ u(x, y) = cos(\pi x) cos(\pi y) $ on $ \partial \Omega $" + "\n" +
+#              r"$ \Omega = (0, 1) \times (0, 1) $"
+#              )
 ax.set_xlabel("x")
 ax.set_ylabel("y")
 cbar.set_label("u")
