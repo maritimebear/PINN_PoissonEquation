18b64706df113dde9fad8a1ae7f160cff93f2183
John Mampilli <jsmampilli@gmail.com>
2023-09-13 10:19:41 +0200

    Changed reference data plot for uniform appearance

--- plot_data.py
+++ plot_data.py
@@ -11,12 +11,12 @@ inputs, output = ds[:]
 assert output.squeeze().ndim == 1  # Confirm output is a vector
 n = int(np.sqrt(len(output)))
 
-fig = plt.figure(figsize=(10, 10))
+fig = plt.figure(figsize=(8, 8))
 ax = fig.add_subplot(1, 1, 1)
 cs = ax.contourf(*[arr.reshape(n, n) for arr in (inputs[:, 0], inputs[:, 1], output)])
 cbar = fig.colorbar(cs)
 
-ax.set_title("Reference solution (ground truth)\n") # +
+ax.set_title("Reference solution (ground truth)") #\n") +
 #              r"$ -\Delta u(x,y) = 2 \pi ^{2} cos(\pi x) cos(\pi y) $ in $ \Omega $" + "\n" +
 #              r"$ u(x, y) = cos(\pi x) cos(\pi y) $ on $ \partial \Omega $" + "\n" +
 #              r"$ \Omega = (0, 1) \times (0, 1) $"
